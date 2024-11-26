16c7ab1f72aae6173327b5f2768c971179fa90fd
Asif-102 <asif1zero2@gmail.com>
2024-11-26 18:16:57 +0600

    changed banner

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing() {
           <div className="mx-auto grid max-w-[40rem] grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-5">
             <div className="flex flex-col lg:pb-6 lg:col-span-2 justify-center">
               <h1 className="mt-4 text-6xl font-extrabold leading-none tracking-tight text-slate-900 sm:text-5xl sm:leading-[3.5rem]">
-                Protocol
+                DevsRocket
               </h1>
 
               <p className="mt-6 text-base leading-7 text-slate-700">
