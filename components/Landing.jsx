99e0dfc03eae72ea2babf7f7a3f8b97f69bafc72
Asif-102 <asif1zero2@gmail.com>
2024-11-26 18:40:19 +0600

    update content

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -13,9 +13,7 @@ export default function Landing() {
               </h1>
 
               <p className="mt-6 text-base leading-7 text-slate-700">
-                It does not matter if you have an API if nobody knows how to use
-                it. Teach people the ins and outs of OAuth 2.0 and JWTs in style
-                with Protocol, a beautiful API documentation template.
+                Developers On Rocket
               </p>
 
               <div className="mt-10 flex flex-wrap gap-4">
