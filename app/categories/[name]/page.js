a1858a424ed5c65c94dceb63755a30e673cfe0f8
Asif-102 <asif1zero2@gmail.com>
2024-12-07 14:45:41 +0600

    use dynamic title

--- app/categories/[name]/page.js
+++ app/categories/[name]/page.js
@@ -2,6 +2,15 @@ import ContentDisplay from "@/components/ContentDisplay";
 import { getDocuments } from "@/lib/doc";
 import { getDocumentsByCategory } from "@/utils/doc-util";
 
+export async function generateMetadata({ params: { name } }) {
+  const docs = getDocuments();
+  const matchedDocs = getDocumentsByCategory(docs, name)[0];
+
+  return {
+    title: matchedDocs.category,
+  };
+}
+
 export function generateStaticParams() {
   const docs = getDocuments();
 
